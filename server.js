0aa962b59d6e8aa01dac5ab50e7a967b41858af6
Horacio23 <horacio.delgado23@gmail.com>
2016-04-11 15:32:21 -0400

    added bootstrap to the project

--- server.js
+++ server.js
@@ -3,10 +3,12 @@ var express = require('express');
 var app = express();
 var port = Number(process.env.PORT || 4000);
 
-app.use(express.static(__dirname + '/public'));
+app.use(express.static('./public'));
+app.set('views', './src/views');
+app.set('view engine', 'ejs');
 
 app.get('/', function(req, res){
-    res.send('Hello');
+    res.render('index');
 });
 
 app.listen(port, function(){
